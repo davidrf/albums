f73f83ce070d40d78fe7012bcd8d93d0305cfa77
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2016-10-23 22:54:03 -0400

    Initialize repository

---
+++ index.ios.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  AppRegistry,
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
+
+AppRegistry.registerComponent('albums', () => App);
