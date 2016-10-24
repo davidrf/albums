1a37db5aab264e5e36d5955aaa0694bee4dbeb56
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2016-10-24 13:15:43 -0400

    Style Album Detail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = ({ title, artist, url, image }) => {
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  }
+};
+
+const AlbumDetail = ({ title, artist, url, image, thumbnail_image: uri }) => {
+  const { headerContentStyle, thumbnailStyle } = styles;
+  const source = { uri };
+
   return (
     <View>
       <Card>
         <CardSection>
-          <Text>{title}</Text>
+          <View>
+            <Image style={thumbnailStyle} source={source} />
+          </View>
+          <View style={headerContentStyle}>
+            <Text>{title}</Text>
+            <Text>{artist}</Text>
+          </View>
         </CardSection>
       </Card>
     </View>
